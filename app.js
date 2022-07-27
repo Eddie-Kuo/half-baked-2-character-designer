92d96d9cd6f3beca8adc288d498d06b3f94e125d
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-07-27 18:12:21 -0500

    add the feature where selecting a head from the dropdown will update the head image as well as change count for the head

--- app.js
+++ app.js
@@ -10,18 +10,29 @@ const catchphrasesEl = document.getElementById('catchphrases');
 const catchphraseInput = document.getElementById('catchphrase-input');
 const catchphraseButton = document.getElementById('catchphrase-button');
 
+let headChanges = 0;
+let middleChanges = 0;
+let bottomChanges = 0;
+
+let characterCatchphrase = [];
+
 // set state for how many times the user changes the head, middle, and bottom
 // set state for all of the character's catchphrases
 
 headDropdown.addEventListener('change', () => {
     // get the value of the head dropdown
-
+    const headSelection = headDropdown.value;
     // increment the head change count state
-    
+    headChanges++;
     // update the dom for the head (use style.backgroundImage on the headEl div instead of trying to set the .src -- it's NOT an img tag!)
-
+    headEl.style.backgroundImage = `url(./assets/${headSelection}-head.png)`;
     // update the stats to show the new count (call displayStats() to do this work)
+    resultsDisplay();
 });
+
+function resultsDisplay() {
+    reportEl.textContent = `You have changed the head ${headChanges} times, ${middleChanges} times, and the ${bottomChanges} times`
+}
 
 
 middleDropdown.addEventListener('change', () => {
